d31604e11aea1180b67c21ea96beda38f49c6e96
guineveresaenger <guineveresaenger@yahoo.com>
2016-09-27 15:58:14 -0700

    Baseline. I think.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'list#index'
+  root 'tasks#index'
 
-get '/list' => 'list#index'
+get '/tasks' => 'tasks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
