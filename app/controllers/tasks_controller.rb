6ad8ceca2f76158c89dcd8d615e6e35a46356280
guineveresaenger <guineveresaenger@yahoo.com>
2016-09-30 15:22:08 -0700

    Cleaned up the completed_at function, deleted done attribute, and added a time_field to the new and edit methods.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,18 +19,12 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
-    # @task.set_completion_time
-
-    if task_params[:done] == "true"
-      @task.completed_at = Time.now
-    end
     if @task.save
       redirect_to tasks_path
     else
       render :new
     end
 
-    puts "MY DONENESS IS: #{@task.done}"
   end
 
 
@@ -42,20 +36,16 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
 
-
-    # @task.set_completion_time
-    if task_params[:done] == "true"
-      @task.completed_at = Time.now
-    end
-
-
+    # if task_params[:done] == "true"
+    #   @task.completed_at = Time.now
+    # end
 
     if @task.update(task_params)
       redirect_to tasks_path
     else
       render :edit
     end
-    puts "MY DONENESS IS: #{@task.done}"
+
   end
 
 
@@ -69,7 +59,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :done)
+    params.require(:task).permit(:title, :description, :completed_at)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,3 @@
 class Task < ActiveRecord::Base
-  attr_accessor :done
-
-  def set_completion_time
-    if params[:done] == true
-      @task.completed_at = Time.now
-    end
-  end
+  
 end
